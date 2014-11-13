47c7315ac84c3b7bfec65f320a4aa9d8640b91d4
tuckismad <tuckismad@gmail.com>
2014-11-13 19:51:12 +1100

    Added map loading

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "game.h"
 
@@ -10,10 +12,14 @@ void Game::Run()
 	screenDimensions = sf::Vector2i(800, 600);
 	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Zelda Clone");
 
+	if(!tileTexture.loadFromFile("Textures/tiles.png"))
+		std::cout << "Texture file cannot be found!" << std::endl;
+	tile.setTexture(tileTexture);
+
 	timePerFrame = sf::milliseconds(16);
 	timer.restart();
 
-	//LoadMap();
+	LoadMap("Maps/Map1.txt");
 
 	while (window.isOpen())
 	{
@@ -53,7 +59,7 @@ void Game::ProcessEvents()
 			break;
 
 			default:
-			std::cout << "An event has fired which hasn't been added to the process list" << std::endl;
+			//std::cout << "An event has fired which hasn't been added to the process list" << std::endl;
 			break;
 		}
 	}
@@ -66,10 +72,49 @@ void Game::ProcessInput()
 
 void Game::LoadMap(const std::string& fileName)
 {
+	std::cout << "Loading Map...." << std::endl;
+	std::fstream openFile(fileName);
+	std::vector<sf::Vector2i> tempMap;
+	tempMap.clear();
+	map.clear();
 
+	if (openFile.is_open())
+	{
+		while (!openFile.eof())
+		{
+			std::string line, value;
+			std::getline(openFile, line);
+			std::stringstream stream(line);
+			while (std::getline(stream, value, ' '))
+			{
+				if (value.length() == 2)
+				{
+					int x = atoi(value.substr(0, 1).c_str());
+					int y = atoi(value.substr(1, 2).c_str());
+
+					tempMap.push_back(sf::Vector2i(x, y));
+				}
+			}
+
+			if (tempMap.size() > 0)
+			{
+				map.push_back(tempMap);
+				tempMap.clear();
+			}
+		}
+	}
+	std::cout << "Map Loaded!" << std::endl;
 }
 
 void Game::DrawMap()
 {
-
+	for (int i = 0; i < map.size(); i++)
+	{
+		for (int j = 0; j < map[i].size(); j++)
+		{
+			tile.setPosition(j * 32, i * 32);
+			tile.setTextureRect(sf::IntRect(map[i][j].x * 32, map[i][j].y * 32, 32, 32));
+			window.draw(tile);
+		}
+	}
 }
--- src/game.h
+++ src/game.h
@@ -12,10 +12,14 @@ private:
 	void ProcessInput();
 	void LoadMap(const std::string& fileName);
 	void DrawMap();
+	std::vector<std::vector<sf::Vector2i>> map;
+	sf::Sprite tile;
+	sf::Texture tileTexture;
 	sf::RenderWindow window;
 	sf::Vector2i screenDimensions;
 	sf::Clock timer;
 	sf::Time timePerFrame;
+
 };
 
 
