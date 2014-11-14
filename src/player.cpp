c3534371dbd7dd2a6d1b23cbdf4b8b6e220f8383
Bahbi <superbahbi@gmail.com>
2014-11-14 01:26:49 -0800

    Change player to PANDA

--- src/player.cpp
+++ src/player.cpp
@@ -12,7 +12,7 @@ void Player::Run()
 	int i = 50;
 	int direction = 1;
 
-	if(!tileTexture.loadFromFile("Textures/player.png"))
+	if(!tileTexture.loadFromFile("Textures/panda.png"))
 		std::cout << "Texture file cannot be found!" << std::endl;
 	tile.setTexture(tileTexture);
 	std::cout << "Texture Loaded!" << std::endl;
@@ -36,6 +36,7 @@ void Player::LoadPlayer()
 void Player::DrawPlayer(sf::RenderWindow& window)
 {
 	tile.setPosition(500,500);
+	tile.setTextureRect(sf::IntRect(0, 0, TILE_SIZE, TILE_SIZE));
 	window.draw(tile);
 }
 
--- src/player.h
+++ src/player.h
@@ -5,18 +5,18 @@ class Player
 {
 public:
 	void Run();
-	void DrawPlayer(sf::RenderWindow& window);	
+	void DrawPlayer(sf::RenderWindow& window);
 private:
+	static const int TILE_SIZE = 32;
 	void Render();
 	void Update();
-	void LoadPlayer();	
+	void LoadPlayer();
 	void ProcessInput();
 	void CheckCollision();
 	void GetPos();
 	void Move();
 	sf::Sprite tile;
 	sf::Texture tileTexture;
-	// sf::RenderWindow window;
 };
 
 
