d5c59a32c52530b9afe142d74c528272ac13e0e7
tuckismad <tuckismad@gmail.com>
2014-11-15 07:47:33 +1100

    Corrected animation order

--- src/player.cpp
+++ src/player.cpp
@@ -17,27 +17,27 @@ void Player::Run(sf::Vector2i screenDimensions)
 	 // set up the animations for all four directions (set spritesheet and push frames)
 
     walkingAnimationDown.setSpriteSheet(tileTexture);
-    walkingAnimationDown.addFrame(sf::IntRect( 2 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
-    walkingAnimationDown.addFrame(sf::IntRect( 1 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
     walkingAnimationDown.addFrame(sf::IntRect( 0 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationDown.addFrame(sf::IntRect( 1 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationDown.addFrame(sf::IntRect( 2 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
 
 
     walkingAnimationLeft.setSpriteSheet(tileTexture);
-    walkingAnimationLeft.addFrame(sf::IntRect( 2 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
-    walkingAnimationLeft.addFrame(sf::IntRect( 1 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
     walkingAnimationLeft.addFrame(sf::IntRect( 0 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationLeft.addFrame(sf::IntRect( 1 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationLeft.addFrame(sf::IntRect( 2 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
 
 
     walkingAnimationRight.setSpriteSheet(tileTexture);
-    walkingAnimationRight.addFrame(sf::IntRect( 2 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
-    walkingAnimationRight.addFrame(sf::IntRect( 1 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
     walkingAnimationRight.addFrame(sf::IntRect( 0 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationRight.addFrame(sf::IntRect( 1 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationRight.addFrame(sf::IntRect( 2 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
 
 
     walkingAnimationUp.setSpriteSheet(tileTexture);
-    walkingAnimationUp.addFrame(sf::IntRect( 2 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
-    walkingAnimationUp.addFrame(sf::IntRect( 1 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
     walkingAnimationUp.addFrame(sf::IntRect( 0 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationUp.addFrame(sf::IntRect( 1 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    walkingAnimationUp.addFrame(sf::IntRect( 2 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
 
 
 
